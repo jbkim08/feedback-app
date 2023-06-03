0614a1ff8f5a88fa160bfaff6aeb40dcedad5f1b
jbkim08 <jbkim08@gmail.com>
2023-06-03 16:54:50 +0900

    스피너 추가

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,16 +1,19 @@
 import { useContext } from 'react';
 import FeedbackItem from './FeedbackItem';
 import FeedbackContext from '../context/FeedbackContext';
+import Spinner from './shared/Spinner';
 
 function FeedbackList() {
-	const { feedback, handleDelete } = useContext(FeedbackContext);
-	if (!feedback || feedback.length === 0) {
+	const { feedback, isLoading } = useContext(FeedbackContext);
+	if (!isLoading && (!feedback || feedback.length === 0)) {
 		return <p>피드백 데이터가 없습니다.</p>;
 	}
-	return (
+	return isLoading ? (
+		<Spinner />
+	) : (
 		<div className='feedback-list'>
 			{feedback.map((item) => (
-				<FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+				<FeedbackItem key={item.id} item={item} />
 			))}
 		</div>
 	);
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,26 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: '컨텍스트 아이템 1 입니다.',
-      rating: 10
-    },
-    {
-      id: 2,
-      text: '컨텍스트 아이템 2 입니다.',
-      rating: 9
-    },
-    {
-      id: 3,
-      text: '컨텍스트 아이템 3 입니다.',
-      rating: 8
-    }
-  ]);
-
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false
   });
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+  // 피드벡데이터 받기
+  const fetchFeedback = async () => {
+    const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`);
+    const data = await response.json();
+
+    setFeedback(data);
+    setIsLoading(false);
+  };
 
   //아이템 추가
   const addFeedback = (newFeedback) => {
@@ -58,6 +53,7 @@ export const FeedbackProvider = ({ children }) => {
     value={{
       feedback,
       feedbackEdit,
+      isLoading,
       setFeedback,
       addFeedback,
       deleteFeedback,
