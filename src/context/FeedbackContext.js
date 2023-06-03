e5ec025b45cff9332b8daae58ce679ead0f872d7
jbkim08 <jbkim08@gmail.com>
2023-06-03 18:05:05 +0900

    프록시 설정

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -14,7 +14,7 @@ export const FeedbackProvider = ({ children }) => {
   }, []);
   // 피드벡데이터 받기
   const fetchFeedback = async () => {
-    const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`);
+    const response = await fetch(`/feedback?_sort=id&_order=desc`);
     const data = await response.json();
 
     setFeedback(data);
@@ -23,7 +23,7 @@ export const FeedbackProvider = ({ children }) => {
 
   //아이템 추가
   const addFeedback = async (newFeedback) => {
-    const response = await fetch('http://localhost:5000/feedback', {
+    const response = await fetch('/feedback', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -38,13 +38,13 @@ export const FeedbackProvider = ({ children }) => {
   //아이템 삭제
   const deleteFeedback = async (id) => {
     if (window.confirm('정말로 삭제할까요?')) {
-      await fetch(`http://localhost:5000/feedback/${id}`, { method: 'DELETE' });
+      await fetch(`/feedback/${id}`, { method: 'DELETE' });
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
   //수정후 저장하기
   const updateFeedback = async (id, updItem) => {
-    const response = await fetch(`http://localhost:5000/feedback/${id}`, {
+    const response = await fetch(`/feedback/${id}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
